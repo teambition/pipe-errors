1c5fac7829070626cb5f3286993321442ee1f88f
Tejesh Mehta <tejesh.mehta@gmail.com>
2019-07-26 23:25:34 -0700

    Improved typescript definition

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,35 @@
 import { EventEmitter } from 'events'
 
-export default function pipeErrors<T = EventEmitter>(
-  ...args: Array<T> | Array<Array<T>>
-): T
+type EE = EventEmitter
+
+function pipeErrors<A = EE>(a: A): A
+function pipeErrors<A = EE, B = EE>(a: A, b: B): B
+function pipeErrors<A = EE, B = EE, C = EE>(a: A, b: B, c: C): C
+function pipeErrors<A = EE, B = EE, C = EE, D = EE>(a: A, b: B, c: C, d: D): D
+function pipeErrors<A = EE, B = EE, C = EE, D = EE, E = EE>(
+  a: A,
+  b: B,
+  c: C,
+  d: D,
+  e: E,
+): E
+function pipeErrors<A = EE, B = EE, C = EE, D = EE, E = EE, F = EE>(
+  a: A,
+  b: B,
+  c: C,
+  d: D,
+  e: E,
+  f: F,
+): F
+function pipeErrors<A = EE>(...a: [A]): A
+function pipeErrors<A = EE, B = EE>(...a: [A, B]): B
+function pipeErrors<A = EE, B = EE, C = EE>(...a: [A, B, C]): C
+function pipeErrors<A = EE, B = EE, C = EE, D = EE>(...a: [A, B, C, D]): D
+function pipeErrors<A = EE, B = EE, C = EE, D = EE, E = EE>(
+  ...a: [A, B, C, D, E]
+): E
+function pipeErrors<A = EE, B = EE, C = EE, D = EE, E = EE, F = EE>(
+  ...a: [A, B, C, D, E, F]
+): F
+
+export default pipeErrors
